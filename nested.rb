6215c0df10f11ab3de722ee24ab0c7881ef34975
Thomas Edwards <thce28@gmail.com>
2019-02-11 15:31:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -124,7 +124,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+     programmer_hash[:dennis_ritchie][:languages][] = "Assembly"
   
     programmer_hash
 end
